58b216f8a7239211c92b878acd12f348d120a75b
Glitch (hello-webpage) <none>
2021-01-15 13:16:12 +0000

    ⛵️🚧 Checkpoint ./style.css:27408933/14743 ./index.html:27408933/33577 ./script.js:27408933/2315 ./.glitch-assets:27408933/1248

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+const sliderImgs = document.querySelectorAll(".slider__img");
+const sliderText = document.querySelectorAll(".slider__text-content");
+const btnRight = document.querySelector(".slider__btn-right");
+const btnLeft = document.querySelector(".slider__btn-left");
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+const nextSlide = () => {
+  const currentSlideImg = document.querySelector(".active-photo");
+
+  const currentSlideText = document.querySelector(".active-text");
+  currentSlideText.classList.remove("active-text");
+
+  currentSlideImg.classList.remove("active-photo");
+
+  if (
+    currentSlideImg.nextElementSibling &&
+    currentSlideImg.nextElementSibling.classList.contains("slider__img")
+  ) {
+    currentSlideImg.nextElementSibling.classList.add("active-photo");
+    currentSlideText.nextElementSibling.classList.add("active-text");
+  } else {
+    sliderImgs[0].classList.add("active-photo");
+    sliderText[0].classList.add("active-text");
+  }
+};
+const prevSlide = () => {
+  const currentSlideImg = document.querySelector(".active-photo");
+
+  const currentSlideText = document.querySelector(".active-text");
+  currentSlideText.classList.remove("active-text");
+
+  currentSlideImg.classList.remove("active-photo");
+
+  if (
+    currentSlideImg.previousElementSibling &&
+    currentSlideImg.previousElementSibling.classList.contains("slider__img")
+  ) {
+    currentSlideImg.previousElementSibling.classList.add("active-photo");
+    currentSlideText.previousElementSibling.classList.add("active-text");
+  } else {
+    sliderImgs[sliderImgs.length - 1].classList.add("active-photo");
+    sliderText[sliderText.length - 1].classList.add("active-text");
+  }
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
+
+// navigation
+const openBtn = document.querySelector(".nav__open-btn");
+const closeBtn = document.querySelector(".nav__close-btn");
+const navWrapper = document.querySelector(".nav__list-wrapper");
+
+openBtn.addEventListener("click", () => {
+  navWrapper.classList.add("open");
+  document.body.classList.add("dark");
+});
+closeBtn.addEventListener("click", () => {
+  navWrapper.classList.remove("open");
+  document.body.classList.remove("dark");
+});
